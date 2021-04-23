c9ed0c0a148da06939159088120a05fa6982986d
paolo-motta <paolo_motta@hotmail.com>
2021-04-23 12:43:49 +0200

    pulizia

--- masterchat_server.py
+++ masterchat_server.py
@@ -26,10 +26,7 @@ def clientthread(conn):
 
     global CLIENT
     result=""
-    '''
-    VERIFICARE I DATI DI CONNESSIONE RICEVUTI
-    E MEMORIZZARLI
-    '''
+
     data = conn.recv(1024)
     #print('\r\nData: ' + data.decode())
     #print(json.loads(data.decode()))
@@ -105,13 +102,6 @@ def clientthread(conn):
             s.close
             break
 
-        '''
-
-        conn.sendall(b"Ti rispondo con quello che mi hai inviato: " + data)
-        conn.send(b"Digita qualcosa - \'exit\' per uscire: ")
-
-        '''
-
     conn.close()
 
 while 1:
@@ -123,47 +113,3 @@ while 1:
 
 s.close()
 
-
-'''
-Input da riga di comando
-• L’input da riga di comando può essere catturato tramite la lista
-sys.argv:
-IP = str(sys.argv[1])
-PORT = int(sys.argv[2])
-NICKNAME = str(sys.argv[3])Caratteri e stringhe
-
-• Per togliere eventuali caratteri di fine stringa al funzione rstrip() può
-essere usata
-
-• Per recuperare la posizione di un determinato carattere all’interno di
-una stringa, ad esempio per esaminare il messaggio ricevuto, si può
-usare la funzione index():
-pos = data.index('|')
-
-• Data una posizione all’interno di una stringa, una sottostringa può
-essere isolata nel seguente modo:
-• pos = data.index('|') # ho una stringa “ciao|pippo”
-• sottostringa = data[:pos] # isolo “ciao”
-• sottostringa = data[pos+1:] # isolo “pippo”Gestire la stampa in maniera ordinata
-• Per gestire la stampa in maniera incrementale si puo’ usare la
-sys.stdout.write() che puo’ essere usata per stampare sottostringhe o
-caratteri singoli e gestire anche il momento in cui tutti i caratteri
-vengono mostrati:
-• sys.stdout.write('\n'+NICKNAME+'>')
-• sys.stdout.flush() # Stampa tutto a videoVariabili condivise tra thread
-• Per avere delle variabili condivise tra uno o più thread delle variabili
-globali possono essere utilizzate
-
-• Queste variabili possono essere accedute anche all’interno di un
-thread (o in generale una funzione) usando la parola chiave global,
-esempio:
-def recv_msg():
-global remote_host
-global remote_portParsare i comandi o i messaggi ricevuti
-
-• Il parsing dei comandi o dei messaggi ricevuti può essere fatto nella
-seguente maniera:
-if user_input[:9] == "!connect ":
-name = user_input[9:].rstrip()
-server_s.sendall("who "+str(name))
-'''
