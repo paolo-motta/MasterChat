1ee6030e7b6a022df972c750739badf7458798ff
paolo-motta <82809615+paolo-motta@users.noreply.github.com>
2021-05-10 12:17:28 +0200

    Update masterchat_client.py

--- masterchat_client.py
+++ masterchat_client.py
@@ -1,15 +1,15 @@
 '''
->>> from cryptography.hazmat.primitives import hashes
->>> from cryptography.hazmat.primitives.asymmetric import ec
->>> from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 >>> private_key = ec.generate_private_key(ec.SECP384R1())
 >>> peer_public_key = ec.generate_private_key(ec.SECP384R1()).public_key()
 >>> shared_key = private_key.exchange(ec.ECDH(), peer_public_key)
 >>> derived_key = HKDF(algorithm=hashes.SHA256(), length=32, salt=None, info=b'handshake data',).derive(shared_key)
 '''
 
-import socket, sys, json
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 from _thread import *
+import socket, sys, json
 
 #memorizzo dati del client e inizializzo variabili
 NICK = str(sys.argv[1])
